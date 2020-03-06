6e686834efe35e44c78e90cdccf4ed15e70d37f8
Alexander Filippov <agoxandr@gmail.com>
2020-03-06 04:46:00 +0100

    update

--- Language/Tests/testTranslator.php
+++ Language/Tests/testTranslator.php
@@ -2,6 +2,6 @@
 include(__DIR__ . "/../translator.php");
 use Agoxandr\Language\Translator;
 
-$translator = new Translator(__DIR__ . "/test.csv");
+$translator = new Translator(__DIR__ . "/test", array("en"));
 $translator->getText("spawn");
 $translator->echoText("spawn");
--- Language/translator.php
+++ Language/translator.php
@@ -5,9 +5,22 @@ class Translator
 {
     private $sheet = array();
 
-    public function __construct(string $path, int $maxTextLength = 0)
+    public function __construct(string $pathNoFilenameEnd, array $supportedLanguages = array("de"), int $maxTextLength = 0)
     {
-        if (($handle = fopen($path, "r")) !== false) {
+        $lang = $supportedLanguages[0];
+        if (!empty($_GET["lang"])) {
+            $getLang = $_GET["lang"];
+            if (in_array($getLang, $supportedLanguages)) {
+                $lang = $getLang;
+            }
+        }
+        if (!empty($_COOKIE["lang"])) {
+            $cookieLang = $_COOKIE["lang"];
+            if (in_array($cookieLang, $supportedLanguages)) {
+                $lang = $cookieLang;
+            }
+        }
+        if (($handle = fopen($pathNoFilenameEnd . "-" . $lang . ".csv", "r")) !== false) {
             while (($data = fgetcsv($handle, $maxTextLength, ",")) !== false) {
                 $this->sheet[$data[0]] = $data[1];
             }
